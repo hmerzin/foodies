e1aa5c958ec61cefd1cc911b8c56b2be9a8e1c36
Mathias Kools <kools.m@husky.neu.edu>
2019-02-10 01:09:13 -0500

    Harden libs against weird input.

--- selection/openstreetmaps.py
+++ selection/openstreetmaps.py
@@ -1,6 +1,9 @@
 import requests
 import json
 import functools
+import logging
+
+logger = logging.getLogger(__name__)
 
 OSRMKEY = "5b3ce3597851110001cf62488dd14a1df3a24e80a7289a189b60edf0"
 OSRMrequestendpoint = "https://api.openrouteservice.org/matrix"
@@ -10,19 +13,65 @@ requestparams = {
 'profile': 'driving-car'}
 
 def addDrivingTime(restaurants, srcLon, srcLat):
-    if (len(restaurants) > 49):
+    if  len(restaurants) == 0:
+        logger.warning("Attempted to retrieve the driving distance of an empty list of restaurants, are you okay?")
+        # I'm following the design recipe here wow.
+        return restaurants
+    elif (len(restaurants) > 49):
+        # The osrm has a limit of 50 locations. We add one location for ourself.
+        logger.warning("Warning you are creating more than one request by asking for more than 49 restaurants.")
         return addDrivingTime(restaurants[0:49], srcLon, srcLat) + addDrivingTime(restaurants[49:], srcLon, srcLat)
     else:
         params = requestparams.copy()
+        # We must make the first location our street address to request
+        # distances to it.
         locations = "{},{}|".format(srcLon, srcLat)
+        # Ugly code to make restaurants appear in the shape {lon},{lat}|... etc.
         otherlocations = "|".join(map(
             lambda i: "{},{}".format(i["longitude"],i["latitude"]), restaurants))
+        # We add the weird code to our street address location. Note that now
+        # the index of each restaurant is its index in restaurants + 1. The
+        # offset is from our current location.
         params["locations"] = locations + otherlocations
+        # Destinations specifies that we wish to only distances from each location
+        # to the first location.
         params["destinations"] = '0'
-        r = requests.get(OSRMrequestendpoint, params=params)
-        data = r.json()
-        # We depend on the same order of restaurants being conserved.
-        for i, restaurant in enumerate(restaurants):
-            restaurant["drivingTime"] = data.get("durations")[i + 1][0]
-            restaurants[i] = restaurant
+        try:
+            r = requests.get(OSRMrequestendpoint, params=params)
+            success = r.raise_for_status()
+            data = r.json()
+
+        except requests.exceptions.RequestException as requesterr:
+            if requesterr is requests.exceptions.HTTPerror:
+                if requesterr.response.status_code == 403:
+                    logger.error("Key is not authorized for openrouteservice!" +
+                    " This could mean that a daily quota has been reached.")
+                elif requesterr.response.status_code == 413 or requesterr.response.status_code == 6004:
+                    logger.error("Hit capacity of openrouteservice! This should" +
+                        "of not happened! This is because we limit requests to" +
+                        "49 of them.")
+                else:
+                    logger.error("There was a non-distinct http error. Error {}".format())
+            else:
+                logger.error("There was a non-descript error oh no!")
+        if success == False:
+            for i, restaurant in enumerate(restaurants):
+                # We fall back to a min wait time for the restaurant.
+                restaurant["drivingTime"] = restaurant["minWaitTime"]
+                restaurants[i] = restaurant # Not really sure how mutation
+                # works here in for loops and stuff so I'm trying to be safe.
+        else:
+            # We depend on the same order of restaurants being conserved.
+            for i, restaurant in enumerate(restaurants):
+                drivingTime = data.get("durations")[i + 1][0]
+                if drivingTime is None:
+                    # If JSON returns that the route is null, signifying that no
+                    # route could be found. Then we will return a high time to avoid
+                    # picking from it.
+                    logger.error("Received null route from openrouteservice, from " +
+                        "(lat,lng) {},{} to {},{}".format(srcLat,srcLon,restaurant['latitude'],restaurant['longitude']))
+                    restaurant["drivingTime"] = restaurant["minWaitTime"]
+                else:
+                    restaurant["drivingTime"] = data.get("durations")[i + 1][0]
+                    restaurants[i] = restaurant
         return restaurants
--- selection/yelpratings.py
+++ selection/yelpratings.py
@@ -1,6 +1,10 @@
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 from fuzzywuzzy import fuzz
+from multiprocessing import Pool
+import logging
+
+logger = logging.getLogger(__name__)
 
 # TEMP
 import sys
@@ -20,10 +24,18 @@ client = Client(
     fetch_schema_from_transport=True,
 )
 
-
-def addYelpRatings(restaurants):
-    if (len(restaurants) > 10):
-        return addYelpRatings(restaurants[0:10]) + addYelpRatings(restaurants[10:])
+# Generate and gather gql query results for a given restaurant list.
+# This may time out, so please use addYelpRatings which will split up
+# the work in a safer ... arbitrary way ...
+# This function will take a list of restaurants to produce
+# a list of ratings. The point of this strategy is that it will
+# become easier to collect all of the mapped slices together at the
+# end to calculate for example ... an average.
+def _getRestaurantsRatings(restaurants):
+    if len(restaurants) == 0:
+        # Don't continue! We don't want to craft an empty gql query that will
+        # just be stupid.
+        return []
     else:
         graphqlquery = '{'
         for i, restaurant in enumerate(restaurants):
@@ -38,18 +50,46 @@ def addYelpRatings(restaurants):
                     name
                     rating
                 }}
-                }},
+                }}
             """.format(i, restaurant["name"], restaurant["streetAddress"],
-                       restaurant["latitude"], restaurant["longitude"])
-        graphqlquery += "\n }"
+            restaurant["latitude"], restaurant["longitude"])
+        graphqlquery+= "\n }"
         data = client.execute(gql(graphqlquery))
+        restaurantmapped = []
         for i, restaurant in enumerate(restaurants):
             if "restaurant{}".format(i) in data and int(data["restaurant{}".format(i)]["total"]) > 0:
                 matchedRestaurant = max(data["restaurant{}".format(i)]["business"],
-                                        key=lambda r: (fuzz.ratio(restaurant["name"], r["name"])))
-                restaurants[i]["rating"] = matchedRestaurant["rating"]
+                    key=lambda r: (fuzz.ratio(restaurant["name"], r["name"])))
+                restaurantmapped.append(matchedRestaurant["rating"])
             else:
                 # We just have to skip if it's not there!
-                # We give it a 2.0 rating.
-                restaurants[i]["rating"] = 2.0
+                restaurantmapped.append(None)
+        return restaurantmapped
+
+# This function adds yelp ratings through manageable slices.
+def addYelpRatings(restaurants):
+    slices = []
+    if len(restaurants) == 0:
+        logger.error("We don't want to fetch restaurant ratings for an empty list!" +
+            " then we get an empty slice.")
+        return []
+    else:
+        for i in range(0,len(restaurants),10):
+            slices.append(restaurants[i: max(i+10,len(restaurants))])
+        with Pool(5) as p:
+            restaurantRatings = p.map(_getRestaurantsRatings, slices)
+            N = 0
+            total = 0
+            for i, slice in enumerate(restaurantRatings):
+                for rating in slice:
+                    if rating is None:
+                        pass
+                    else:
+                        N += 1
+                        total += rating
+            average = total / N if N > 1 else 0
+            print(average)
+            for i, slice in enumerate(restaurantRatings):
+                for i2, rating in enumerate(slice):
+                    restaurants[i*10 + i2]["rating"] = average if restaurantRatings[i][i2] is None else restaurantRatings[i][i2]
         return restaurants
